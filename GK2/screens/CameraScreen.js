afb1de564141b0c26fc1a6242874cf03b52e2614
TRUOB <tr@uob.dk>
2024-10-14 17:06:26 +0200

    cam virker ikke

--- GK2/screens/CameraScreen.js
+++ GK2/screens/CameraScreen.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { CameraType, Camera } from 'expo-camera';
 
 const CameraScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+
+  // Anmod om kamera tilladelser 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  // Hvis kamera-tilladelser ikke er givet
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  // Skift mellem kamera-front og -bag
+  const toggleCameraType = () => {
+    setType(type === CameraType.back ? CameraType.front : CameraType.back);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Kamera</Text>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 };
@@ -13,12 +44,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#fff',
   },
   text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'white',
   },
 });
 
